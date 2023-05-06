4e0e4a2ad7bd4a5784e387898afff880273e93e6
Last Capricorn <48774366+LastCapricorn@users.noreply.github.com>
2023-05-06 03:48:55 +0200

    Have nice Cards and nice Table Form for new books in card mode Issues with read switch

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 ## Third project of The Odin Project's Full Stack JavaScript Course
 
-I read the assignment on TOP for this project and I don't know why, suddenly felt like knowing absolutely nothing about web development. So I started some exercises in a folder I called "library gymnastics". I had a pretty good idea what my library should be able to do and as almost always, I had set too high standards for myself. But that's how the library gymnastics quickly became a big part of this project...
-
+I read the assignment on TOP for this project and I don't know why, suddenly felt like knowing absolutely nothing about web development. So I started some exercises in a folder I called "library gymnastics". I had a pretty good idea what my library should be able to do and as almost always, I had set too high standards for myself. But that's how the "library gymnastics" quickly became a big part of this project...
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const defaultLibrary = [
   {
     title: "Brave New World",
@@ -82,188 +84,382 @@ const defaultLibrary = [
   },
 ]
 
-const headings = ["Title", "Author", "Category", "Year", "Pages", "Read", ""]
-
 class Library {
   constructor(name) {
     this.name = name
-    this.library = []
+    this.data = []
   }
 
-  addToLibrary = (book) => {
-    if (
-      this.library.every(
-        (b) => (b.title && b.author) !== (book.title && book.author)
-      )
-    ) {
-      this.library.push(book)
-      this.saveToLocalStorage()
-    } else {
-      return `"${book.title}" by ${book.author} already exists in this library!`
-    }
+  importLibrary = () => {
+    this.data = JSON.parse(localStorage.getItem(this.name))
   }
 
-  removeFromLibrary = (book) => {
-    if (this.library.includes(book)) {
-      this.library.splice(this.library.indexOf(book))
-      this.saveToLocalStorage()
-    } else {
-      return `There is no Book '${book.title}' in this library!`
-    }
-  }
-
-  importLibrary = (name) => {
-    this.library = JSON.parse(localStorage.getItem(name))
-  }
-
-  saveToLocalStorage = () => {
+  exportLibrary = () => {
     try {
-      localStorage.setItem(this.name, JSON.stringify(this.library))
+      localStorage.setItem("current", this.name)
+      localStorage.setItem(this.name, JSON.stringify(this.data))
     } catch (err) {
       return "Sorry, apparently the localStorage isn't available to save the library!"
     }
   }
 
-  showLibraryAsTable = () => {
-    const container = document.querySelector("main")
-    const bookTable = document.createElement("table")
-    const bookHead = document.createElement("thead")
-    const bookBody = document.createElement("tbody")
-
-    container.classList.remove("card")
-    container.classList.add("table")
-
-    headings.forEach((elem) => {
-      const bookHeading = document.createElement("th")
-      bookHeading.textContent = elem
-      bookHead.appendChild(bookHeading)
-    })
-    bookTable.appendChild(bookHead)
-
-    this.library.forEach((book) => {
-      const bookRow = document.createElement("tr")
-      for (const key in book) {
-        if (Object.hasOwnProperty.call(book, key)) {
-          const bookDat = document.createElement("td")
-          if (!(key === "read")) {
-            bookDat.textContent = book[key]
-            bookRow.appendChild(bookDat)
-          } else {
-            const checkboxRead = document.createElement("input")
-            checkboxRead.setAttribute("type", "checkbox")
-            checkboxRead.setAttribute("id", "read")
-            checkboxRead.setAttribute("name", "read")
-            checkboxRead.setAttribute("value", book[key])
-            if (book[key]) {
-              checkboxRead.setAttribute("checked", "checked")
-            }
-            bookDat.appendChild(checkboxRead)
-            bookRow.appendChild(bookDat)
-          }
-        }
-      }
-      const buttonCell = document.createElement("td")
-      const buttonRemove = document.createElement("button")
-      const trashBinSvg = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "svg"
+  addToLibrary = (book) => {
+    if (
+      this.data.every(
+        (b) => (b.title && b.author) !== (book.title && book.author)
       )
-      const trashBinPath1 = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "path"
-      )
-      const trashBinPath2 = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "path"
-      )
-      buttonRemove.setAttribute("type", "button")
-      buttonRemove.setAttribute("id", "remove")
-      buttonRemove.setAttribute("name", "remove")
-      trashBinSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg")
-      trashBinSvg.setAttribute("viewBox", "0 0 256 256")
-      trashBinSvg.setAttribute("fill", "currentColor")
-      trashBinPath1.setAttribute(
-        "d",
-        "M200,56V208a8,8,0,0,1-8,8H64a8,8,0,0,1-8-8V56Z"
-      )
-      trashBinPath1.setAttribute("opacity", "0.2")
-      trashBinPath2.setAttribute(
-        "d",
-        "M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"
-      )
-      trashBinSvg.appendChild(trashBinPath1)
-      trashBinSvg.appendChild(trashBinPath2)
-      buttonRemove.appendChild(trashBinSvg)
-      buttonCell.appendChild(buttonRemove)
-      bookRow.appendChild(buttonCell)
-      bookBody.appendChild(bookRow)
-      bookTable.appendChild(bookBody)
-    })
-    container.appendChild(bookTable)
+    ) {
+      this.data.push(book)
+      this.exportLibrary()
+    } else {
+      return `"${book.title}" by ${book.author} already exists in this library!`
+    }
   }
 
-  showLibraryAsCardSet = () => {
-    const container = document.querySelector("main")
+  removeFromLibrary = (bookTitle) => {
+    const bookIndex = this.data.findIndex((entry) => entry.title === bookTitle)
+    this.data.splice(bookIndex, 1)
+    this.exportLibrary()
+  }
+}
 
-    container.classList.remove("table")
-    container.classList.add("card")
+class Book {
+  constructor(
+    title,
+    author = 'N/A',
+    category = 'N/A',
+    year = "N/A",
+    pages = "N/A",
+    read = false
+  ) {
+    this.title = title
+    this.author = author
+    this.category = category
+    this.year = year
+    this.pages = pages
+    this.read = read
+  }
+}
 
-    this.library.forEach((book) => {
+const headings = ["Title", "Author", "Category", "Year", "Pages", "Read", ""]
+const container = document.querySelector("main")
+const newLibraryButton = document.querySelector("body nav div button")
+let currentLibrary
+
+function attachForm() {
+  const formCard = document.createElement("div")
+  formCard.classList.add("form")
+  const formForm = document.createElement("form")
+  formForm.setAttribute('action', '')
+  formForm.setAttribute('method', 'get')
+  const titleLabel = document.createElement("label")
+  const titleInput = document.createElement("input")
+  titleLabel.setAttribute("for", "book-title")
+  titleLabel.textContent = "Title"
+  titleInput.setAttribute("type", "text")
+  titleInput.setAttribute("id", "book-title")
+  titleInput.setAttribute("name", "book-title")
+  titleInput.setAttribute("required", "")
+
+  const authorLabel = document.createElement("label")
+  const authorInput = document.createElement("input")
+  authorLabel.setAttribute("for", "book-author")
+  authorLabel.textContent = "Author"
+  authorInput.setAttribute("type", "text")
+  authorInput.setAttribute("id", "book-author")
+  authorInput.setAttribute("name", "book-author")
+
+  const categoryLabel = document.createElement("label")
+  const categoryInput = document.createElement("input")
+  categoryLabel.setAttribute("for", "book-category")
+  categoryLabel.textContent = "Category"
+  categoryInput.setAttribute("type", "text")
+  categoryInput.setAttribute("id", "book-category")
+  categoryInput.setAttribute("name", "book-category")
+
+  const yearLabel = document.createElement("label")
+  const yearInput = document.createElement("input")
+  yearLabel.setAttribute("for", "book-year")
+  yearLabel.textContent = "Year"
+  yearInput.setAttribute("type", "number")
+  yearInput.setAttribute("id", "book-year")
+  yearInput.setAttribute("name", "book-year")
+  yearInput.setAttribute("min", "0")
+  yearInput.setAttribute("max", new Date().getFullYear())
+  yearInput.value = new Date().getFullYear()
+
+  const pagesLabel = document.createElement("label")
+  const pagesInput = document.createElement("input")
+  pagesLabel.setAttribute("for", "book-pages")
+  pagesLabel.textContent = "Pages"
+  pagesInput.setAttribute("type", "number")
+  pagesInput.setAttribute("id", "book-pages")
+  pagesInput.setAttribute("name", "book-pages")
+  pagesInput.setAttribute("min", "0")
+  pagesInput.setAttribute("max", 30000)
+
+  const readLabel = document.createElement("label")
+  const readInput = document.createElement("input")
+  readLabel.setAttribute("for", "book-read")
+  readLabel.textContent = "Read"
+  readInput.setAttribute("type", "checkbox")
+  readInput.setAttribute("id", "book-read")
+  readInput.setAttribute("name", "book-read")
+  readInput.setAttribute("value", false)
+  readInput.classList.add("switch")
+  readInput.addEventListener('change' , () => readInput.setAttribute('value', readInput.checked))
+  const cancelButton = document.createElement("button")
+  const addButton = document.createElement("button")
+  cancelButton.setAttribute("type", "clear")
+  cancelButton.textContent = "cancel"
+  addButton.setAttribute("type", "submit")
+  addButton.textContent = "add"
+
+  formForm.appendChild(titleLabel)
+  formForm.appendChild(titleInput)
+  formForm.appendChild(authorLabel)
+  formForm.appendChild(authorInput)
+  formForm.appendChild(categoryLabel)
+  formForm.appendChild(categoryInput)
+  formForm.appendChild(yearLabel)
+  formForm.appendChild(yearInput)
+  formForm.appendChild(pagesLabel)
+  formForm.appendChild(pagesInput)
+  formForm.appendChild(readLabel)
+  formForm.appendChild(readInput)
+  formForm.appendChild(cancelButton)
+  formForm.appendChild(addButton)
+  formCard.appendChild(formForm)
+  container.insertAdjacentElement("afterbegin", formCard)
+}
+
+function attachRemoveButton(title) {
+  const removeButton = document.createElement("button")
+  const trashBinSvg = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
+  )
+  const trashBinPath1 = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  )
+  const trashBinPath2 = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  )
+
+  removeButton.setAttribute("type", "button")
+  removeButton.setAttribute("class", "remove")
+  removeButton.setAttribute("name", "remove")
+  removeButton.setAttribute("value", title)
+  trashBinSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+  trashBinSvg.setAttribute("viewBox", "0 0 256 256")
+  trashBinSvg.setAttribute("fill", "currentColor")
+  trashBinPath1.setAttribute(
+    "d",
+    "M200,56V208a8,8,0,0,1-8,8H64a8,8,0,0,1-8-8V56Z"
+  )
+  trashBinPath1.setAttribute("opacity", "0.2")
+  trashBinPath2.setAttribute(
+    "d",
+    "M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"
+  )
+  trashBinSvg.appendChild(trashBinPath1)
+  trashBinSvg.appendChild(trashBinPath2)
+  removeButton.appendChild(trashBinSvg)
+  return removeButton
+}
+
+function attachSwitch(key, iD) {
+  const switchLabel = document.createElement("label")
+  const switchInput = document.createElement("input")
+  switchInput.setAttribute("type", "checkbox")
+  switchInput.setAttribute("id", iD)
+  switchInput.setAttribute("class", "switch")
+  switchInput.setAttribute("name", "switch")
+  switchInput.setAttribute("value", key)
+  if (key) {
+    switchInput.setAttribute("checked", "")
+  }
+  switchInput.addEventListener('change', (ev) => {
+    switchInput.setAttribute('value', switchInput.checked)
+    const bookIndex = currentLibrary.data.findIndex((entry) => entry.title === ev.target.id)
+    currentLibrary.data[bookIndex].read = ev.target.value
+    currentLibrary.exportLibrary()
+    })
+  switchLabel.setAttribute("for", iD)
+  switchLabel.appendChild(switchInput)
+  return switchLabel
+}
+
+function showLibraryAsCardSet(lib) {
+  container.classList.remove("table")
+  document.querySelector("body").classList.remove("table")
+  container.classList.add("card")
+  if (lib.data !== []) {
+    for (let book = 0; book < lib.data.length; book++) {
       const card = document.createElement("div")
-      for (const key in book) {
-        if (Object.hasOwnProperty.call(book, key)) {
+      card.setAttribute("data-title", lib.data[book].title)
+
+      for (const key in lib.data[book]) {
+        if (Object.hasOwnProperty.call(lib.data[book], key)) {
           const cardHeading = document.createElement("h2")
           const cardText = document.createElement("p")
+          const cardRuler = document.createElement("hr")
+          cardHeading.setAttribute("class", key)
           cardHeading.textContent = key
           cardHeading.textContent = cardHeading.textContent.replace(
             cardHeading.textContent[0],
             cardHeading.textContent[0].toUpperCase()
           )
-          cardText.textContent = book[key]
           card.appendChild(cardHeading)
-          card.appendChild(cardText)
+          cardText.setAttribute("class", key)
+          if (key === "read") {
+            card.appendChild(
+              attachSwitch(lib.data[book][key], lib.data[book].title)
+            )
+          } else {
+            cardText.textContent = lib.data[book][key]
+            card.appendChild(cardText)
+          }
+          if (!(key === "read")) {
+            card.appendChild(cardRuler)
+          }
         }
       }
+      const removeButton = attachRemoveButton(lib.data[book].title)
+      removeButton.addEventListener("click", () => {
+        lib.removeFromLibrary(removeButton.value)
+        const removeCard = document.querySelector(
+          `div[data-title = '${removeButton.value}']`
+        )
+        container.removeChild(removeCard)
+      })
+      card.appendChild(removeButton)
       container.appendChild(card)
-    })
+    }
   }
 }
 
-// class Book {
-//   constructor(
-//     title,
-//     author = "N/A",
-//     category = "n/A",
-//     year = "N/A",
-//     pages = "N/A",
-//     read = false
-//   ) {
-//     this.title = title
-//     this.author = author
-//     this.category = category
-//     this.year = year
-//     this.pages = pages
-//     this.read = read
-//   }
-// }
+function showLibraryAsTable(lib) {
+  const libraryTable = document.createElement("table")
+  const tableHead = document.createElement("thead")
+  const tableBody = document.createElement("tbody")
 
-const defLib = new Library("defaultLibrary")
-defLib.library = defaultLibrary
+  container.classList.remove("card")
+  document.querySelector("body").classList.add("table")
+  container.classList.add("table")
 
-defLib.showLibraryAsCardSet()
+  headings.forEach((elem) => {
+    const tableHeading = document.createElement("th")
+    tableHeading.textContent = elem
+    tableHead.appendChild(tableHeading)
+  })
+  libraryTable.appendChild(tableHead)
 
-document.querySelector("#library-style").addEventListener("change", () => {
-  document.querySelector("#library-style").classList.toggle("checked")
+  lib.data.forEach((book) => {
+    const tableRow = document.createElement("tr")
+    const buttonCell = document.createElement("td")
+
+    for (const key in book) {
+      if (Object.hasOwnProperty.call(book, key)) {
+        const tableDat = document.createElement("td")
+        if (!(key === "read")) {
+          tableDat.textContent = book[key]
+          tableRow.appendChild(tableDat)
+        } else {
+          tableDat.appendChild(attachSwitch(book.read, book.title))
+          tableRow.appendChild(tableDat)
+        }
+      }
+    }
+
+    buttonCell.appendChild(attachRemoveButton(book.title))
+    tableRow.appendChild(buttonCell)
+    tableBody.appendChild(tableRow)
+    libraryTable.appendChild(tableBody)
+  })
+  container.appendChild(libraryTable)
+
+  const switchInput = document.querySelectorAll(".switch-container .switch")
+  switchInput.forEach((sw) =>
+    sw.addEventListener("change", () => {
+      sw.classList.toggle("checked")
+      if (sw.classList.contains("checked")) {
+        sw.value = "true"
+      } else {
+        sw.value = "false"
+      }
+    })
+  )
+}
+
+function initializeLibrary() {
+  const setSelection = document.querySelector("body nav div select")
+  const setOptions = document.createElement("option")
+  if (setSelection.hasAttribute("disabled")) {
+    setSelection.removeAttribute("disabled")
+    newLibraryButton.removeAttribute("disabled")
+  }
+
+  if (localStorage && localStorage.length !== 0) {
+    for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i) !== "current") {
+        setOptions.textContent = localStorage.key(i)
+        if (localStorage.key(i) === localStorage.getItem("current")) {
+          setOptions.setAttribute("selected", "")
+        }
+        setSelection.appendChild(setOptions)
+      }
+    }
+    currentLibrary = new Library(localStorage.getItem("current"))
+    currentLibrary.importLibrary()
+    showLibraryAsCardSet(currentLibrary)
+  } else if (localStorage && localStorage.length === 0) {
+    const exampleLibrary = new Library("Example Library")
+    setOptions.textContent = exampleLibrary.name
+    setOptions.setAttribute("selected", "")
+    setSelection.appendChild(setOptions)
+    exampleLibrary.data = defaultLibrary
+    exampleLibrary.exportLibrary()
+    showLibraryAsCardSet(exampleLibrary)
+  } else {
+    newLibraryButton.setAttribute("disabled", "")
+    setSelection.setAttribute("disabled", "")
+    const exampleLibrary = new Library("Example Library")
+    exampleLibrary.data = defaultLibrary
+    showLibraryAsCardSet(exampleLibrary)
+  }
+}
+
+function toggleLibraryDisplayMode() {
   document.querySelector(".card-style").classList.toggle("checked")
   document.querySelector(".table-style").classList.toggle("checked")
-  if (document.querySelector("main").classList.contains("card")) {
-    document
-      .querySelector("main")
-      .querySelectorAll("div")
-      .forEach((div) => document.querySelector("main").removeChild(div))
-    defLib.showLibraryAsTable()
-  } else {
+  if (document.querySelector("main").classList.contains("table")) {
     document.querySelector("main").removeChild(document.querySelector("table"))
-    defLib.showLibraryAsCardSet()
+    showLibraryAsCardSet(currentLibrary)
+    attachForm()
+  } else {
+    document
+      .querySelectorAll("main div")
+      .forEach((div) => document.querySelector("main").removeChild(div))
+    showLibraryAsTable(currentLibrary)
   }
+}
+
+initializeLibrary()
+attachForm()
+
+document.querySelector("#display-style").addEventListener("change", toggleLibraryDisplayMode)
+document.querySelector('button[type="submit"]').addEventListener('click', (ev) => {
+  ev.preventDefault()
+  const titleBook = document.querySelector('input[id="book-title"]').value
+  const authorBook = document.querySelector('input[id="book-author"]').value
+  const categoryBook = document.querySelector('input[id="book-category"]').value
+  const yearBook = document.querySelector('input[id="book-year"]').value
+  const pagesBook = document.querySelector('input[id="book-pages"]').value
+  const readBook = document.querySelector('input[id="book-read"]').value
+  const newBook = new Book(titleBook,authorBook,categoryBook,yearBook,pagesBook,readBook)
+  currentLibrary.addToLibrary(newBook)
+
 })
